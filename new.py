6b2150945996060ea89147bde0539d7be4d5e987
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-04-02 20:09:52 +0200

    Update new.py

--- new.py
+++ new.py
@@ -14,11 +14,9 @@ from fastapi.exception_handlers import (
 )
 import random
 
-
 app = FastAPI()
 
 teams = {}
-
 
 projects = {}
 
@@ -26,35 +24,26 @@ assigned_projects = {}
 
 team_id_idle = {}
 
-
 team_projects = {}
-
 
 def delete_all():
    teams.clear()
 
-
 @app.get("/")
 def root():
     return {"message": "Hello!"}
-
-
 
 @app.get("/status")
 def get_status():
     if (teams, projects):
         raise HTTPException(status_code=200)
     
-
-
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
     )
-
-
 
 @app.put("/teams")
 def put_teams(team: List[Team]):
@@ -69,24 +58,18 @@ def put_teams(team: List[Team]):
         dev_list.append(value)
     return teams
 
-
-
 @app.post("/project")
 def add_project(project: Project):
     projects[project.id] = project.devs_needed
 
     for team_id, team_devs in teams.items():
-
         assigned_projects1 = team_projects.get(team_id)
         assigned_projects1 = assigned_projects1 if assigned_projects1 is not None else list()
         used_devs_count = sum([projects.get(project_id) for project_id in assigned_projects1])
-
-
         if ((team_devs - used_devs_count) >= project.devs_needed):
             assigned_projects1.append(project.id)
             team_projects[team_id] = assigned_projects1
             break
-
     return team_projects
 
 
